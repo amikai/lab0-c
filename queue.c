8988dee4edf1373799cb402cf073c776652786ad
amikai <as23041248@gmail.com>
2018-09-26 11:23:06 +0800

    Implement intialization of queue

--- queue.c
+++ queue.c
@@ -25,8 +25,10 @@
 queue_t *q_new()
 {
     queue_t *q = malloc(sizeof(queue_t));
-    /* What if malloc returned NULL? */
-    q->head = NULL;
+    if (q) {
+        q->head = q->tail = NULL;
+        q->size = 0;
+    }
     return q;
 }
 
