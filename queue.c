8965b9c37a8dc7f19ffeb9f2d67a95852eac31b8
amikai <as23041248@gmail.com>
2018-09-27 15:24:19 +0800

    Implement q_insert_head to insert element at head

--- queue.c
+++ queue.c
@@ -57,13 +57,27 @@ void q_free(queue_t *q)
  */
 bool q_insert_head(queue_t *q, char *s)
 {
-    list_ele_t *newh;
-    /* What should you do if the q is NULL? */
-    newh = malloc(sizeof(list_ele_t));
-    /* Don't forget to allocate space for the string and copy it */
-    /* What if either call to malloc returns NULL? */
+    if (!q)
+        return false;
+
+    list_ele_t *newh = (list_ele_t *) malloc(sizeof(list_ele_t));
+    if (!newh)
+        return false;
+
+    newh->value = (char *) malloc(strlen(s) + 1);
+    if (!newh->value) {
+        free(newh);
+        return false;
+    }
+    strcpy(newh->value, s);
+
+    if (q->size == 0) {
+        q->tail = newh;
+    }
     newh->next = q->head;
     q->head = newh;
+    q->size++;
+
     return true;
 }
 
